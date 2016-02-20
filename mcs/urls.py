3f43c30d8321f2d2265f015667c0fa7ceefec29a
Hridoy Dutta <hridaydutta123@gmail.com>
2016-02-20 23:11:59 +0530

    PrettyPhoto, MorrisJs real-data added

--- mcs/urls.py
+++ mcs/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^sync/$', views.sync, name='sync'),
     url(r'^analysis/graphical$', views.graphicalAnalysis, name='graphicalAnalysis'),
+	url(r'^analysis/tables$', views.tabularAnalysis, name='tabularAnalysis'),
+    url(r'^filemanager/imageView$', views.imageView, name='imageView'),
+    url(r'^filemanager/videoView$', views.videoView, name='videoView')
+
 ]
