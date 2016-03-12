93db18c24b3f345880f547290340bd32b7172069
Hridoy Dutta <hridaydutta123@gmail.com>
2016-03-13 01:08:33 +0530

    Offline Map Integrated

--- mcs/views.py
+++ mcs/views.py
@@ -24,18 +24,22 @@ def insertToSpecificTable(filePath, fileType, fileModelObj):
 		messageSplit = message.split(':')
 
 		if(messageSplit[0] == 'Health'):
-			strTxtObj = Health(Type=messageSplit[0],Quantity=messageSplit[2],File=fileModelObj)
+			strTxtObj = Health(Type=messageSplit[1],Quantity=messageSplit[2],File=fileModelObj)
 			strTxtObj.save()
 		if(messageSplit[0] == 'Shelter'):
-			strTxtObj = Shelter(Type=messageSplit[0],Quantity=messageSplit[2], File=fileModelObj)
+			strTxtObj = Shelter(Type=messageSplit[1],Quantity=messageSplit[2], File=fileModelObj)
+			strTxtObj.save()    	
+		
+		if(messageSplit[0] == 'Victims'):
+			strTxtObj = Victims(Type=messageSplit[1],Quantity=messageSplit[2], File=fileModelObj)
 			strTxtObj.save()    	
 		
 		if(messageSplit[0] == 'Food'):
-			strTxtObj = Food(Type=messageSplit[0],Quantity=messageSplit[2], File=fileModelObj)
+			strTxtObj = Food(Type=messageSplit[1],Quantity=messageSplit[2], File=fileModelObj)
 			strTxtObj.save()
 		
 		if(messageSplit[0] == 'Areas'):
-			strTxtObj = Food(Type=messageSplit[0],Quantity=messageSplit[2], File=fileModelObj)
+			strTxtObj = Food(Type=messageSplit[1],Quantity=messageSplit[2], File=fileModelObj)
 			strTxtObj.save()
 		
 	return 1
@@ -71,10 +75,15 @@ def checkAndInsert(filePath):
 			return 1
 	return 0
 
-# Create your views here.
+def fileStruct():
+	fileLatLangTable = {}
+	fileList = Files.objects.values_list();
+	for item in fileList:
+		print 
 
-
+# Create your views here
 def index(request):
+	fileStruct()
 	context = {}
 	context['countAllFiles'] = Files.objects.all().count()
 	context['countIMG'] = Files.objects.filter(Type='IMG').count()
@@ -82,7 +91,8 @@ def index(request):
 	context['countSMS'] = Files.objects.filter(Type='SMS').count()
 	context['countTXT'] = Files.objects.filter(Type='TXT').count()
 	context['countAUD'] = Files.objects.filter(Type='AUD').count()
-  
+	allFiles = Files.objects.all()
+	context['latlong'] = [(i.lat, i.lon) for i in allFiles]
 	
 	if context['countAllFiles'] > 0:
 		context['audioDistribution'] = (
@@ -117,12 +127,37 @@ def graphicalAnalysis(request):
 	context['countTXT'] = Files.objects.filter(Type='TXT').count()
 	context['countAUD'] = Files.objects.filter(Type='AUD').count()
 	context['DateTime'] = Files.objects.values_list('DateTime', flat=True)
+	
+	context['Health'] =  Health.objects.values_list()
+	context['HealthData'] = {}
+	for x in context['Health']:
+		context['HealthData'][x[1]] = x[2]
+	print context['HealthData']
+
+	context['Food'] =  Food.objects.values_list()
+
+	context['FoodData'] = {}
+	for x in context['Food']:
+		context['FoodData'][x[1]] = x[2]
+	print context['FoodData']
+
+
+	context['Victims'] = Victims.objects.values_list()
+	context['VictimsCount'] = Victims.objects.all().count()
+	print context['Victims']
+	context['VictimData'] = {}
+	for x in context['Victims']:
+		countVictimPerc = (x[2]* 100 / context['VictimsCount'])/2
+		context['VictimData'][x[1]] = countVictimPerc
+
+	print context['VictimData']
+
+
 	context['years'] = list()
 	for years in context['DateTime']:
 		val1 = str(years)
 		
 		context['years'].append(val1[:7])
-		#print(context['years'])
 
 	if context['countAllFiles'] > 0:
 		context['audioDistribution'] = ((context['countAUD'] * 100) /
@@ -156,7 +191,32 @@ def tabularAnalysis(request):
 		val1 = str(years) 
 		context['TtlVal'].append(val1)
 	
+	
+	context['Health'] =  Health.objects.values_list()
+	context['HealthData'] = {}
+	for x in context['Health']:
+		context['HealthData'][x[1]] = x[2]
+	print context['HealthData']
 
+	context['Food'] =  Food.objects.values_list()
+
+	context['FoodData'] = {}
+	for x in context['Food']:
+		context['FoodData'][x[1]] = x[2]
+	print context['FoodData']
+
+
+	context['Victims'] = Victims.objects.values_list()
+	context['VictimsCount'] = Victims.objects.all().count()
+	print context['Victims']
+	context['VictimData'] = {}
+	for x in context['Victims']:
+		countVictimPerc = (x[2])
+		context['VictimData'][x[1]] = countVictimPerc
+
+	print context['VictimData']
+
+	
 	context['TtlFinalVal'] = {j:context['TtlVal'].count(j) for j in context['TtlVal']}
 	print(context['TtlFinalVal'])
    
