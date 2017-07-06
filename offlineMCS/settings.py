086478683941080743881509fa6f17cb2aaf4fcb
Hridoy Dutta <hridaydutta123@gmail.com>
2017-07-06 21:05:04 +0530

    Added config vars

--- offlineMCS/settings.py
+++ offlineMCS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5di&8=tg7k^o$f9wujgp*(o!x5u=!dq$uz8i1g)zf0m57n%qty'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
